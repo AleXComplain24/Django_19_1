a7a95c7f005b7d4f5a7478a874a96b1a71ccab07
AleXComplain24 <alexkorg98@gmail.com>
2024-12-26 08:30:56 +0300

    pgadmin

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_database',  # Укажите имя базы данных
+        'USER': 'postgres',     # Логин для подключения
+        'PASSWORD': '5582',  # Пароль для PostgreSQL
+        'HOST': '127.0.0.1',    # Адрес сервера
+        'PORT': '5432',         # Порт PostgreSQL
     }
 }
 
--- shop/task1/models.py
+++ shop/task1/models.py
@@ -27,3 +27,15 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
